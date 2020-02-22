daf3e89ee2c701042ac9ba96d5b34c6ae5e17d1e
Artur Iatsko <arturyatsko@gmail.com>
2020-02-22 22:51:11 +0000

    Function fixed and code cleaned up

---
+++ fetcher.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const fs = require('fs');
+
+const site = process.argv.slice(2, 3).toString();
+const fileName = process.argv.slice(3, 4).toString();
+
+request(site, (error, response, body) => {
+  fs.writeFile(fileName, body, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    let stats = fs.statSync(fileName);
+    let fileSizeInBytes = stats.size;
+
+    console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${fileName}`);
+
+  });
+});
